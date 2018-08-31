9c1495f0b6b19d796ec1b9b246962798bbce461a
Milene Toazza <oi@mihtoa.ppg.br>
2018-08-31 01:40:08 -0300

    Blackthorn family

---
+++ ex/Blackthorn.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Member from './member'
+
+export default props => (
+  <div>
+      <Member name="Helen" lastName="Blackthorn" />
+      <Member name="Mark" lastName="Blackthorn" />
+      <Member name="Julian" lastName="Blackthorn" />
+      <Member name="Tiberius" lastName="Blackthorn" />
+      <Member name="Livia" lastName="Blackthorn" />
+      <Member name="Drusilia" lastName="Blackthorn" />
+      <Member name="Octavian" lastName="Blackthorn" />
+  </div>
+)
---
+++ ex/second-component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Second = props => (
+  <h1>Second Component</h1>
+)
+
+export const Third = props => {
+  return <h1>Third Component</h1>
+}
+
+// export { Second, Third }

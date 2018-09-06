e5edda4c13db1e7c7ad2699117aa098ff07ef0eb
Milene Toazza <oi@mihtoa.ppg.br>
2018-09-05 21:41:17 -0300

    family, field, classComponent

--- ex/index.jsx
+++ ex/index.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Family from './family'
 import Member from './member'
+import ClassComponent from './classComponent'
+import Field from './field'
 
 ReactDOM.render(
-  <Family>
-    <Member name="Julian" lastName="Blackthorn" />
-  </Family>
-, document.getElementById('app'))
+  <div>
+    <Family lastName="Carstairs">
+      <Member name="Emma" />
+      <Member name="James" />
+    </Family>
+    <Family>
+      <Member name="Julian" lastName="Blackthorn" />
+    </Family>
+    <ClassComponent label="Contador" initialValue={10} />
+    <Field initialValue='Digite sua mensagem...' />
+  </div>
+  , document.getElementById('app'))
